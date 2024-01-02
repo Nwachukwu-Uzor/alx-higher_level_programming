bd045f84e8aa76df64b3b0406cc4cd82019341b5
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-01-02 15:45:13 -0800

    Fix: task 1 - divide by zero error'

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = (int)(str(number)[-1:]) * (number // abs(number))
+last_digit = 0
+if number > 0:
+    last_digit = (int)(str(number)[-1:]) * (number // abs(number))
 print(f"Last digit of {number} is {last_digit}", end = "")
 if last_digit > 5:
     print(f" and is greater than 5")
