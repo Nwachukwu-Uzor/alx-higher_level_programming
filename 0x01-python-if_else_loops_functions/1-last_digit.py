4fb9a17920ad630cdbfbe157c70df13dcb495dda
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-01-02 17:29:35 -0800

    Fix: Linting error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number > 0:
     last_digit = (int)(str(number)[-1:])
 elif number < 0:
     last_digit = (int)(str(number)[-1:]) * -1
-print(f"Last digit of {number} is {last_digit}", end = "")
+print(f"Last digit of {number} is {last_digit}", end="")
 if last_digit > 5:
     print(f" and is greater than 5")
 elif last_digit == 0:
