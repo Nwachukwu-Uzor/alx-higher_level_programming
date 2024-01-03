f34f616ce5bb74bd5bab59019a36b8eb767ec61d
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-01-02 19:08:10 -0800

    Fix task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,5 +8,5 @@ def fizzbuzz():
             print("Buzz", end="")
         if i % 3 != 0 and i % 5 != 0:
             print(i, end="")
-        if i < 100:
-           print(" ", end="")
+        if i < 99:
+            print(" ", end="")
