baab60ca57cc6d8563252ada02553311fe0661df
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-01-07 10:34:09 -0800

    Fix 5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
 def no_c(my_string):
-      copy = my_string
-      return copy.translate({ord('c'): None}).translate({ord('C'): None})
+    copy = my_string
+    count_c = copy.count('c')
+    count_C = copy.count('C')
+    if count_c > 0:
+        for i in range(count_c):
+            copy.remove('c')
+    if count_C > 0:
+        for i in range(count_C):
+            copy.remove('C')
+    return copy
