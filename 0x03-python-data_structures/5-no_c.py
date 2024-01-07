ac301a240c7613772bead22fc128e110efaa4cd8
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-01-07 10:40:42 -0800

    Fix 5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 def no_c(my_string):
-    copy = my_string
+    copy = list(my_string)
     count_c = copy.count('c')
     count_C = copy.count('C')
     if count_c > 0:
         for i in range(count_c):
-            copy.remove('c')
+            pos = copy.index('c')
+            copy[pos] = ''
     if count_C > 0:
         for i in range(count_C):
-            copy.remove('C')
-    return copy
+            pos = copy.index('C')
+            copy[pos] = ''
+    return ''.join(copy)
