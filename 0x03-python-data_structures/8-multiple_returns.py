3f3d54cb0313e6030ba85230275209c8939f7a9f
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-01-08 02:08:06 -0800

    Fix task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 
 def multiple_returns(sentence):
     if len(sentence) <= 0:
-        return None
-    return ("{:d}".format(len(sentence)), sentence[0])
+        return (0, None)
+    return (len(sentence), sentence[0])
