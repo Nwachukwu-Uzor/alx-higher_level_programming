54308f03459ed414d21fefec86034e236cbfb896
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-01-10 12:32:31 -0800

    Fix task 13

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+
 def complex_delete(my_dict, value):
     cpy = my_dict.copy()
     for entry, val in cpy.items():
         if value == val:
             my_dict.pop(entry)
     return my_dict
-
